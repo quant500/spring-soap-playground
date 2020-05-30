564d80024ec6b44f4b637b43af3878ecafbb5d77
quant500 <quanttrader500@gmail.com>
2020-05-30 22:02:23 +0200

    refactorings

--- soap-provider/src/main/java/com/example/producingwebservice/SoapProviderApplication.java
+++ soap-provider/src/main/java/com/example/producingwebservice/SoapProviderApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoapProducerApplication {
+public class SoapProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SoapProducerApplication.class, args);
+        SpringApplication.run(SoapProviderApplication.class, args);
     }
 }
