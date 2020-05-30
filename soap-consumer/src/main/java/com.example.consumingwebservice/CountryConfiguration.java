54ffc960c30c0aa66edc9f824698a3b81278c617
quant500 <quanttrader500@gmail.com>
2020-05-30 22:25:46 +0200

    refactorings

--- soap-consumer/src/main/java/com.example.consumingwebservice/CountryConfiguration.java
+++ soap-consumer/src/main/java/com.example.consumingwebservice/CountryConfiguration.java
@@ -6,20 +6,22 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
 public class CountryConfiguration {
+    public static final String CONTEXT_PATH = "com.example.consumingwebservice.wsdl";
+    public static final String DEFAULT_URI = "http://localhost:8080/ws";
 
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         // this package must match the package in the <generatePackage>
         // specified in pom.xml
-        marshaller.setContextPath("com.example.consumingwebservice.wsdl");
+        marshaller.setContextPath(CONTEXT_PATH);
         return marshaller;
     }
 
     @Bean
     public CountrySoapConsumer countryClient(Jaxb2Marshaller marshaller) {
         CountrySoapConsumer client = new CountrySoapConsumer();
-        client.setDefaultUri("http://localhost:8080/ws");
+        client.setDefaultUri(DEFAULT_URI);
         client.setMarshaller(marshaller);
         client.setUnmarshaller(marshaller);
         return client;
