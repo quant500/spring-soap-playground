3ef0fb4ae887f256a09f1c527c06676e5fb68038
quant500 <quanttrader500@gmail.com>
2020-05-31 21:45:05 +0200

    refactoring

--- soap-provider/src/main/java/com/example/producingwebservice/SoapProviderConfiguration.java
+++ soap-provider/src/main/java/com/example/producingwebservice/SoapProviderConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.xml.xsd.XsdSchema;
 // enables SOAP Web Service features in this Spring Boot application
 @EnableWs
 @Configuration
-public class SoapProviderConfig
+public class SoapProviderConfiguration
         // The WebServiceConfig class extends the WsConfigurerAdapter base class,
         // which configures the annotation-driven Spring-WS programming model.
         extends WsConfigurerAdapter {
